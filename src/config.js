1fd4b4e14c825a6fe19bed6967ae8666f693e311
cristian} <cristianespinosa018@gmail.com>
2024-06-13 21:49:52 -0500

    any

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = window.location.href.includes('localhost') 
                         ? "http://localhost:3000" 
-                        : "https://zg698gwp-3000.use.devtunnels.ms/"
+                        : "https://zg698gwp-3000.use.devtunnels.ms"
